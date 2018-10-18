735bf225922230d900ce9d3b7722d6d79c846fcc
m45rud <m45rud@gmail.com>
2018-10-18 19:39:30 +0700

    Rename database folder

---
+++ database/ams_native.sql
@@ -0,0 +1,242 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 18, 2018 at 07:05 PM
+-- Server version: 5.7.23-0ubuntu0.16.04.1
+-- PHP Version: 7.2.11-2+ubuntu16.04.1+deb.sury.org+1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ams_native`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_disposisi`
+--
+
+CREATE TABLE `tbl_disposisi` (
+  `id_disposisi` int(10) NOT NULL,
+  `tujuan` varchar(250) NOT NULL,
+  `isi_disposisi` mediumtext NOT NULL,
+  `sifat` varchar(100) NOT NULL,
+  `batas_waktu` date NOT NULL,
+  `catatan` varchar(250) NOT NULL,
+  `id_surat` int(10) NOT NULL,
+  `id_user` tinyint(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_instansi`
+--
+
+CREATE TABLE `tbl_instansi` (
+  `id_instansi` tinyint(1) NOT NULL,
+  `institusi` varchar(150) NOT NULL,
+  `nama` varchar(150) NOT NULL,
+  `status` varchar(150) NOT NULL,
+  `alamat` varchar(150) NOT NULL,
+  `kepsek` varchar(50) NOT NULL,
+  `nip` varchar(25) NOT NULL,
+  `website` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `logo` varchar(250) NOT NULL,
+  `id_user` tinyint(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_instansi`
+--
+
+INSERT INTO `tbl_instansi` (`id_instansi`, `institusi`, `nama`, `status`, `alamat`, `kepsek`, `nip`, `website`, `email`, `logo`, `id_user`) VALUES
+(1, 'Dinas Pendidikan Pemuda Dan Olahraga', 'SMK MasRud.com', 'Terakreditasi A', 'Sawahan, Nganjuk, Jawa Timur', 'M. Rudianto', '-', 'https://masrud.com', 'rudi@masrud.com', 'logo.png', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_klasifikasi`
+--
+
+CREATE TABLE `tbl_klasifikasi` (
+  `id_klasifikasi` int(5) NOT NULL,
+  `kode` varchar(30) NOT NULL,
+  `nama` varchar(250) NOT NULL,
+  `uraian` mediumtext NOT NULL,
+  `id_user` tinyint(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_sett`
+--
+
+CREATE TABLE `tbl_sett` (
+  `id_sett` tinyint(1) NOT NULL,
+  `surat_masuk` tinyint(2) NOT NULL,
+  `surat_keluar` tinyint(2) NOT NULL,
+  `referensi` tinyint(2) NOT NULL,
+  `id_user` tinyint(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_sett`
+--
+
+INSERT INTO `tbl_sett` (`id_sett`, `surat_masuk`, `surat_keluar`, `referensi`, `id_user`) VALUES
+(1, 100, 5, 10, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_surat_keluar`
+--
+
+CREATE TABLE `tbl_surat_keluar` (
+  `id_surat` int(10) NOT NULL,
+  `no_agenda` int(10) NOT NULL,
+  `tujuan` varchar(250) NOT NULL,
+  `no_surat` varchar(50) NOT NULL,
+  `isi` mediumtext NOT NULL,
+  `kode` varchar(30) NOT NULL,
+  `tgl_surat` date NOT NULL,
+  `tgl_catat` date NOT NULL,
+  `file` varchar(250) NOT NULL,
+  `keterangan` varchar(250) NOT NULL,
+  `id_user` tinyint(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_surat_masuk`
+--
+
+CREATE TABLE `tbl_surat_masuk` (
+  `id_surat` int(10) NOT NULL,
+  `no_agenda` int(10) NOT NULL,
+  `no_surat` varchar(50) NOT NULL,
+  `asal_surat` varchar(250) NOT NULL,
+  `isi` mediumtext NOT NULL,
+  `kode` varchar(30) NOT NULL,
+  `indeks` varchar(30) NOT NULL,
+  `tgl_surat` date NOT NULL,
+  `tgl_diterima` date NOT NULL,
+  `file` varchar(250) NOT NULL,
+  `keterangan` varchar(250) NOT NULL,
+  `id_user` tinyint(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id_user` tinyint(2) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(35) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `nip` varchar(25) NOT NULL,
+  `admin` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id_user`, `username`, `password`, `nama`, `nip`, `admin`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'M. Rudianto', '-', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_disposisi`
+--
+ALTER TABLE `tbl_disposisi`
+  ADD PRIMARY KEY (`id_disposisi`);
+
+--
+-- Indexes for table `tbl_instansi`
+--
+ALTER TABLE `tbl_instansi`
+  ADD PRIMARY KEY (`id_instansi`);
+
+--
+-- Indexes for table `tbl_klasifikasi`
+--
+ALTER TABLE `tbl_klasifikasi`
+  ADD PRIMARY KEY (`id_klasifikasi`);
+
+--
+-- Indexes for table `tbl_sett`
+--
+ALTER TABLE `tbl_sett`
+  ADD PRIMARY KEY (`id_sett`);
+
+--
+-- Indexes for table `tbl_surat_keluar`
+--
+ALTER TABLE `tbl_surat_keluar`
+  ADD PRIMARY KEY (`id_surat`);
+
+--
+-- Indexes for table `tbl_surat_masuk`
+--
+ALTER TABLE `tbl_surat_masuk`
+  ADD PRIMARY KEY (`id_surat`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_disposisi`
+--
+ALTER TABLE `tbl_disposisi`
+  MODIFY `id_disposisi` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tbl_klasifikasi`
+--
+ALTER TABLE `tbl_klasifikasi`
+  MODIFY `id_klasifikasi` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `tbl_surat_keluar`
+--
+ALTER TABLE `tbl_surat_keluar`
+  MODIFY `id_surat` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `tbl_surat_masuk`
+--
+ALTER TABLE `tbl_surat_masuk`
+  MODIFY `id_surat` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id_user` tinyint(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
