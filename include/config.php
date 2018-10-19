3869fd4534926585a1bfb5f211a3030f41b98f35
m45rud <m45rud@gmail.com>
2018-10-19 10:08:53 +0700

    Change password

--- include/config.php
+++ include/config.php
@@ -1,11 +1,11 @@
 <?php
     $host = "localhost";
     $username = "root";
-    $password = "root";
+    $password = "masrud.com";
     $database = "ams_native";
     $config = mysqli_connect($host, $username, $password, $database);
 
-    if(!$config){
+    if (! $config) {
         die("Koneksi database gagal: " . mysqli_connect_error());
     }
 ?>
