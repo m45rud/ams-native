65fcc726b0d318476c1d13dd5096f04a0355b50f
M. Rudianto <m45rud@gmail.com>
2020-02-29 11:55:22 +0700

    Perbaikan autocomplete – 2020-02-29 11:55:22

--- kode.php
+++ kode.php
@@ -1,38 +1,41 @@
 <?php
-require_once 'include/config.php';
-require_once 'include/functions.php';
-
-// Cegak akses langsung ke source Ajax.
-if ( isset( $_SERVER['HTTP_X_REQUESTED_WITH'] ) && ( $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest' ) ) {
+// Cegah direct akses ajax.
+if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) &&
+         ($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest')) {
 
     // Set header type konten.
     header("Content-Type: application/json; charset=UTF-8");
 
+    require_once "include/config.php";
+    require_once "include/functions.php";
+
     // Koneksi ke database.
-    $conn = conn($host, $username, $password, $database);
+    $conn = mysqli_connect($host, $username, $password, $database);
 
     // Deklarasi variable keyword kode.
     $kode = $_GET["query"];
 
     // Query ke database.
-    $query = $conn->query("SELECT * FROM tbl_klasifikasi WHERE kode LIKE '%$kode%' OR nama LIKE '%$kode%' ORDER BY kode DESC");
-    $result = $query->fetch_all(MYSQLI_ASSOC);
+    $query  = mysqli_query($conn, "SELECT * FROM tbl_klasifikasi
+        WHERE kode LIKE '%$kode%'
+        OR nama LIKE '%$kode%'
+        OR uraian LIKE '%$kode%'");
 
-    // Format bentuk data untuk autocomplete.
-    foreach($result as $data) {
+    if (mysqli_num_rows($query) > 0){
+        // Format bentuk data untuk autocomplete.
+        while ($data = mysqli_fetch_assoc($query)) {
+            $output['suggestions'][] = [
+                'value' => $data['kode'] . " " . $data['nama'],
+                'kode'  => $data['kode']
+            ];
+        }
+    } else {
         $output['suggestions'][] = [
-            'value' => $data['kode'] . " " . $data['nama'],
-            'kode'  => $data['kode']
+            'value' => '',
+            'kode'  => ''
         ];
     }
 
-    if (! empty($output)) {
-        // Encode ke format JSON.
-        echo json_encode($output);
-    }
-
-} else {
-
-    // Tampilkan peringatan.
-    echo 'No direct access source!';
+    // Encode ke json.
+    echo json_encode($output);
 }
